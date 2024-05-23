165b03690e492d284422801c98bd69d44c2fe7eb
ferdaous <ferdaouselsouini0409@gmail.com>
2024-05-23 16:30:58 +0100

    w

--- front-end/src/component/DetailActivities/DetailActivities.jsx
+++ front-end/src/component/DetailActivities/DetailActivities.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 export default function DetailActivities() {
-    const [dat, setDat] = useState([]);
+    const [dat, setDat] = useState({});
+    const [Cin, setCin] = useState([]);
     const { id } = useParams();
 
     useEffect(() => {
@@ -12,19 +13,51 @@ export default function DetailActivities() {
                 const activitesData = await activitesResponse.json();
                 setDat(activitesData);
             } catch (error) {
-                console.error("Error fetching data:", error);
+                console.error("Error fetching activity data:", error);
             }
         };
         fetchData();
     }, [id]);
 
-    return (
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const cinemasResponse = await fetch(`http://127.0.0.1:8000/api/cinemas`);
+                const cinemasData = await cinemasResponse.json();
+                setCin(cinemasData);
+            } catch (error) {
+                console.error("Error fetching cinemas data:", error);
+            }
+        };
+        fetchData();
+    }, []);
 
-        <div>
-            <h1>hello {id}</h1>
-                <div key={dat.id}>
-                    <h1>the {dat.nom}</h1>
+    function detail() {
+        if (parseInt(id) === 1) {
+            return (
+                <div>
+                    {Cin.map((item) => (
+                        <div key={item.id}>
+                            <h1>{item.nom_cinema}</h1>
+                            <p>{item.localisation}</p>
+                            <img src={item.photo} alt="" />
+                        </div>
+                    ))}
                 </div>
+            );
+        } else {
+            return (
+                <div>
+                    <h1>{dat.nom}</h1>
+                    <img src="" alt="" />
+                </div>
+            );
+        }
+    }
+
+    return (
+        <div>
+            {detail()}
         </div>
-    )
+    );
 }
