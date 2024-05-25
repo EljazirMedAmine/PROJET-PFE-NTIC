ab96aac6de16fdeb4927ac343a0faf5889e11df3
ferdaous <ferdaouselsouini0409@gmail.com>
2024-05-25 15:00:06 +0100

    cinema added

--- database/seeders/CinemaSeeder.php
+++ database/seeders/CinemaSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,27 +17,33 @@ class CinemaSeeder extends Seeder
         $cinemas = [
             [
                 'nom_cinema' => 'Cinema Rif',
-                'localisation' => 'https://maps.app.goo.gl/YmAwn3reKFN8DQCQ8',
+                'description' => 'Cinema Rif in Tangier, located in Grand Socco, is a historic theater known for its Art Deco design and cultural significance.',
+                'localisation' => "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3236.654448452593!2d-5.8130882!3d35.7838637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0c7f52c34bd6eb%3A0xa1f5bf6e66c997d4!2sCin%C3%A9ma%20Rif!5e0!3m2!1sen!2sma!4v1716644100006!5m2!1sen!2sma",
                 'photo' => 'https://lh5.googleusercontent.com/p/AF1QipOpiP3drjA8FfZE1x355Ba5Dln9XDncxiKEsbE=w408-h304-k-no',
             ],
             [
                 'nom_cinema' => 'Megarama Goya',
-                'localisation' => 'https://maps.app.goo.gl/4t8vhKH4S1Aj5UPW7',
+                'description' => 'Megarama Goya in Tangier is a modern cinema complex with multiple screens, showing international and local films.',
+                'localisation' => "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3236.8708297690973!2d-5.8098168999999995!3d35.77854920000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0c7fd1b6e291b7%3A0xf8cc4a0807e6999e!2sCin%C3%A9ma%20M%C3%A9garama%20Goya!5e0!3m2!1sen!2sma!4v1716644158636!5m2!1sen!2sma",
                 'photo' => 'https://lh5.googleusercontent.com/p/AF1QipMySVlqSbr6PtuxmtFDOSJl00m0CThdnyTye1dH=w408-h268-k-no',
             ],
             [
                 'nom_cinema' => 'Megarama City Mall',
-                'localisation' => 'https://maps.app.goo.gl/mYZtcDCR34GJS4dH7',
+                'description' => 'Megarama City Mall in Tangier is a modern cinema with multiple screens, located in City Mall.',
+                'localisation' => "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6474.19039701455!2d-5.7875514!3d35.7730378!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0b809c5849a325%3A0x2aa8818fd122a17a!2sMegarama!5e0!3m2!1sen!2sma!4v1716644201645!5m2!1sen!2sma",
                 'photo' => 'https://tanger.megarama.ma/public/contenus/69e8c98eb12348bc9d3b9e1132b98705/images/megarama-citymall769a8--w512.jpg',
             ],
             [
                 'nom_cinema' => 'Roxy',
-                'localisation' => 'https://maps.app.goo.gl/g5nXJ3CvqNmhV7oU6',
+                'description' => '
+                Roxy in Tangier is a historic cinema celebrated for its classic charm and cultural importance.',
+                'localisation' => "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d25895.17593212885!2d-5.8456087!3d35.7779066!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0c7f542cdcfcdd%3A0xd9a9f9e8b9b62bfa!2sCin%C3%A9ma%20Roxy!5e0!3m2!1sen!2sma!4v1716644241526!5m2!1sen!2sma",
                 'photo' => 'https://docs.imperium.plus/files/media-GRGQG-GFRFXRXG-PQQLL-GSMFFW-GFRFGQQPLL-LLPXR-GFRFGQSMRQFW-X-QQXGQFFWQG',
             ],
             [
                 'nom_cinema' => 'Alcazar',
-                'localisation' => 'https://maps.app.goo.gl/xAm5Hpp1E4jH1sre9',
+                'description' => 'Alcazar in Tangier is a historic cinema known for its classic architecture and cultural significance.',
+                'localisation' => "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d103569.71974224875!2d-5.8134603!3d35.7863369!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0c7f4d53b1dea3%3A0x9ff624210a70d56!2sCinema%20Alcazar!5e0!3m2!1sen!2sma!4v1716644278596!5m2!1sen!2sma",
                 'photo' => 'https://i0.wp.com/newstourisme.com/wp-content/uploads/2022/03/IMG_4850.jpg?fit=800%2C533&ssl=1',
             ],
         ];
@@ -44,4 +51,3 @@ class CinemaSeeder extends Seeder
         DB::table('cinemas')->insert($cinemas);
     }
 }
-
--- front-end/src/component/DetailActivities/DetailActivities.jsx
+++ front-end/src/component/DetailActivities/DetailActivities.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
+import "./DetailActivities.css";
+import NavBar from "../NavBar";
 
 export default function DetailActivities() {
     const [dat, setDat] = useState({});
-    const [Cin, setCin] = useState([]);
     const { id } = useParams();
+    const [cinemas, setCinemas] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(1);
+    const [hoverIndex, setHoverIndex] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -18,32 +22,125 @@ export default function DetailActivities() {
         };
         fetchData();
     }, [id]);
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/api/cinemas")
+            .then((response) => response.json())
+            .then((data) => {
+                setCinemas(data);
+                setCurrentIndex(1); // Initialize with the first main item
+            })
+            .catch((error) => console.error("Error fetching cinemas:", error));
+    }, []);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const cinemasResponse = await fetch(`http://127.0.0.1:8000/api/cinemas`);
-                const cinemasData = await cinemasResponse.json();
-                setCin(cinemasData);
-            } catch (error) {
-                console.error("Error fetching cinemas data:", error);
+        const updateCarousel = (direction) => {
+            if (!cinemas.length) return;
+
+            let newIndex;
+            if (direction === "right") {
+                newIndex = (currentIndex + 1) % cinemas.length;
+            } else {
+                newIndex = (currentIndex - 1 + cinemas.length) % cinemas.length;
             }
+            setCurrentIndex(newIndex);
         };
-        fetchData();
-    }, []);
+
+        const leftBtn = document.querySelector("#leftBtn");
+        const rightBtn = document.querySelector("#rightBtn");
+
+        if (leftBtn && rightBtn) {
+            const handleRightClick = () => updateCarousel("right");
+            const handleLeftClick = () => updateCarousel("left");
+            rightBtn.addEventListener("click", handleRightClick);
+            leftBtn.addEventListener("click", handleLeftClick);
+
+            return () => {
+                rightBtn.removeEventListener("click", handleRightClick);
+                leftBtn.removeEventListener("click", handleLeftClick);
+            };
+        }
+    }, [cinemas, currentIndex]);
+
+    const getClassForItem = (index) => {
+        if (index === currentIndex) return "carousel__item--main";
+        if (index === (currentIndex - 1 + cinemas.length) % cinemas.length)
+            return "carousel__item--left";
+        if (index === (currentIndex + 1) % cinemas.length)
+            return "carousel__item--right";
+        return "";
+    };
+
+    const handleMouseEnter = (index) => {
+        setHoverIndex(index);
+    };
+
+    const handleMouseLeave = () => {
+        setHoverIndex(null);
+    };
+
+
+
 
     function detail() {
         if (parseInt(id) === 1) {
             return (
-                <div>
-                    {Cin.map((item) => (
-                        <div key={item.id}>
-                            <h1>{item.nom_cinema}</h1>
-                            <p>{item.localisation}</p>
-                            <img src={item.photo} alt="" />
+                <Fragment>
+                    <div className="parentCinemas">
+                        <h1>Cinemas available</h1>
+                        <p>
+                            Cinemas in Tangier feature a mix of modern multiplexes and historic theaters, showcasing both international films and Moroccan cinema.</p>
+                        <div className="carousel">
+                            {cinemas.map((cinema, index) => (
+                                <div
+                                    key={cinema.id}
+                                    className={`carousel__item ${getClassForItem(
+                                        index
+                                    )}`}
+                                    onMouseEnter={() => handleMouseEnter(index)}
+                                    onMouseLeave={handleMouseLeave}
+                                >
+                                    <img src={cinema.photo} alt={cinema.nom_cinema} />
+                                    <div className="carousel__text">
+                                        {/* <h3>{cinema.nom_cinema}</h3>
+                                    <a
+                                        href={cinema.localisation}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        View Location{" "}
+                                        <i className="bx bx-link-external"></i>
+                                    </a> */}
+
+                                        {hoverIndex === index && (
+                                            <div className="carousel__info">
+                                                <h5>{cinema.nom_cinema}</h5>
+                                                <p>{cinema.description}</p>
+
+                                                <iframe
+                                                    src={cinema.localisation}
+                                                    width={700}
+                                                    height={550}
+                                                    style={{ border: 0 }}
+                                                    allowFullScreen
+                                                    loading="lazy"
+                                                    referrerPolicy="no-referrer-when-downgrade"
+                                                />
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            ))}
+                            <div className="carousel__btns">
+                                <button className="carousel__btn" id="leftBtn">
+                                    <i className="bx bx-left-arrow-alt"></i>
+                                </button>
+                                <button className="carousel__btn" id="rightBtn">
+                                    <i className="bx bx-right-arrow-alt"></i>
+                                </button>
+                            </div>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                </Fragment>
             );
         } else {
             return (
@@ -55,8 +152,10 @@ export default function DetailActivities() {
         }
     }
 
+
     return (
         <div>
+            <NavBar/>
             {detail()}
         </div>
     );
