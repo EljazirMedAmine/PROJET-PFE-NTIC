6c5b9a7012d7a450a7caad39e10baf00bb2326d2
DESKTOP-BIHSHM9\POP <eljazirmohamedamine06@gmail.com>
2024-05-27 21:25:17 +0100

    home page

--- front-end/src/component/NavBarHome.jsx
+++ front-end/src/component/NavBarHome.jsx
@@ -3,50 +3,53 @@ import { Link } from "react-router-dom"; // Add this import statement
 import "./NavBarHome.css"; // Import CSS file
 
 const NavBarHome = () => {
-  return (
-    <>
-      <header className="header">
-        <div className="background-image"></div> {/* Div for background image */}
-        <div className="logo">
-          <Link to="/HomePage">
-            <img src="/pic/blacklogo.png" alt="Logo" className="logo-image" />
-          </Link>
-        </div>
-        <nav className="navigation2">
-          <ul>
-            <li>
-              <Link to="/HomePage" className="active">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/Activitiespage">Activities</Link>
-            </li>
-            <li>
-              <Link to="/RestaurantPage">Restaurants</Link>
-            </li>
-            <li>
-              <Link to="/Hotels">Hotels</Link>
-            </li>
-            <li>
-              <Link to="/Spots">Spots</Link>
-            </li>
-            <li>
-              <Link to="/aboutus">About Us</Link>
-            </li>
-            <li>
-              <Link to="/ContactUs">Contact Us</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="signin-button">
-          <Link to={"/signin"}>
-            <button>Sign In</button>
-          </Link>
-        </div>
-      </header>
-    </>
-  );
+    return (
+        <>
+            <header className="header">
+                <div className="logo">
+                    <Link to="/">
+                        <img
+                            src="/pic/whitelogo.png"
+                            alt="Logo"
+                            className="logo-image"
+                        />
+                    </Link>
+                </div>
+                <nav className="navigation2">
+                    <ul>
+                        <li>
+                            <Link to="/" className="active">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/Activitiespage">Activities</Link>
+                        </li>
+                        <li>
+                            <Link to="/RestaurantPage">Restaurants</Link>
+                        </li>
+                        <li>
+                            <Link to="/Hotels">Hotels</Link>
+                        </li>
+                        <li>
+                            <Link to="/Spots">Spots</Link>
+                        </li>
+                        <li>
+                            <Link to="/aboutus">About Us</Link>
+                        </li>
+                        <li>
+                            <Link to="/ContactUs">Contact Us</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="signin-button">
+                    <Link to={"/signin"}>
+                        <button>Sign In</button>
+                    </Link>
+                </div>
+            </header>
+        </>
+    );
 };
 
 export default NavBarHome;
